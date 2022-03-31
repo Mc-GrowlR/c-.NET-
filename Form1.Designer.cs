c0b2f014b43d4fd66b4e5317174ed1e5d0612443
Mc-GrowlR <growlr@126.com>
2022-03-31 18:16:19 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace jisuanqi
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btnjian = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btrncheng = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btnchu = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btndot = new System.Windows.Forms.Button();
+            this.btnresult = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.btnpluss = new System.Windows.Forms.Button();
+            this.lblShow = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn7
+            // 
+            this.btn7.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            resources.ApplyResources(this.btn7, "btn7");
+            this.btn7.Name = "btn7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.UseWaitCursor = true;
+            this.btn7.Click += new System.EventHandler(this.ButtonNum_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            resources.ApplyResources(this.btn8, "btn8");
+            this.btn8.Name = "btn8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.UseWaitCursor = true;
+            this.btn8.Click += new System.EventHandler(this.ButtonNum_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            resources.ApplyResources(this.btn9, "btn9");
+            this.btn9.Name = "btn9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.UseWaitCursor = true;
+            this.btn9.Click += new System.EventHandler(this.ButtonNum_Click);
+            // 
+            // btnjian
+            // 
+            resources.ApplyResources(this.btnjian, "btnjian");
+            this.btnjian.Name = "btnjian";
+            this.btnjian.UseVisualStyleBackColor = true;
+            this.btnjian.UseWaitCursor = true;
+            this.btnjian.Click += new System.EventHandler(this.ButtonOperator_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            resources.ApplyResources(this.btn4, "btn4");
+            this.btn4.Name = "btn4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.UseWaitCursor = true;
+            this.btn4.Click += new System.EventHandler(this.ButtonNum_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            resources.ApplyResources(this.btn5, "btn5");
+            this.btn5.Name = "btn5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.UseWaitCursor = true;
+            this.btn5.Click += new System.EventHandler(this.ButtonNum_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            resources.ApplyResources(this.btn6, "btn6");
+            this.btn6.Name = "btn6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.UseWaitCursor = true;
+            this.btn6.Click += new System.EventHandler(this.ButtonNum_Click);
+            // 
+            // btrncheng
+            // 
+            resources.ApplyResources(this.btrncheng, "btrncheng");
+            this.btrncheng.Name = "btrncheng";
+            this.btrncheng.UseVisualStyleBackColor = true;
+            this.btrncheng.UseWaitCursor = true;
+            this.btrncheng.Click += new System.EventHandler(this.ButtonOperator_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            resources.ApplyResources(this.btn1, "btn1");
+            this.btn1.Name = "btn1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.UseWaitCursor = true;
+            this.btn1.Click += new System.EventHandler(this.ButtonNum_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            resources.ApplyResources(this.btn2, "btn2");
+            this.btn2.Name = "btn2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.UseWaitCursor = true;
+            this.btn2.Click += new System.EventHandler(this.ButtonNum_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            resources.ApplyResources(this.btn3, "btn3");
+            this.btn3.Name = "btn3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.UseWaitCursor = true;
+            this.btn3.Click += new System.EventHandler(this.ButtonNum_Click);
+            // 
+            // btnchu
+            // 
+            resources.ApplyResources(this.btnchu, "btnchu");
+            this.btnchu.Name = "btnchu";
+            this.btnchu.UseVisualStyleBackColor = true;
+            this.btnchu.UseWaitCursor = true;
+            this.btnchu.Click += new System.EventHandler(this.ButtonOperator_Click);
+            // 
+            // button13
+            // 
+            resources.ApplyResources(this.button13, "button13");
+            this.button13.Name = "button13";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.UseWaitCursor = true;
+            // 
+            // btn0
+            // 
+            this.btn0.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            resources.ApplyResources(this.btn0, "btn0");
+            this.btn0.Name = "btn0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.UseWaitCursor = true;
+            this.btn0.Click += new System.EventHandler(this.ButtonNum_Click);
+            // 
+            // btndot
+            // 
+            resources.ApplyResources(this.btndot, "btndot");
+            this.btndot.Name = "btndot";
+            this.btndot.UseVisualStyleBackColor = true;
+            this.btndot.UseWaitCursor = true;
+            this.btndot.Click += new System.EventHandler(this.ButtonNum_Click);
+            // 
+            // btnresult
+            // 
+            resources.ApplyResources(this.btnresult, "btnresult");
+            this.btnresult.Name = "btnresult";
+            this.btnresult.UseVisualStyleBackColor = true;
+            this.btnresult.UseWaitCursor = true;
+            this.btnresult.Click += new System.EventHandler(this.ButtonOperator_Click);
+            // 
+            // button17
+            // 
+            resources.ApplyResources(this.button17, "button17");
+            this.button17.Name = "button17";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.UseWaitCursor = true;
+            // 
+            // button18
+            // 
+            resources.ApplyResources(this.button18, "button18");
+            this.button18.Name = "button18";
+            this.button18.UseVisualStyleBackColor = true;
+            this.button18.UseWaitCursor = true;
+            // 
+            // button19
+            // 
+            resources.ApplyResources(this.button19, "button19");
+            this.button19.Name = "button19";
+            this.button19.UseVisualStyleBackColor = true;
+            this.button19.UseWaitCursor = true;
+            // 
+            // btnpluss
+            // 
+            resources.ApplyResources(this.btnpluss, "btnpluss");
+            this.btnpluss.Name = "btnpluss";
+            this.btnpluss.UseVisualStyleBackColor = true;
+            this.btnpluss.UseWaitCursor = true;
+            this.btnpluss.Click += new System.EventHandler(this.ButtonOperator_Click);
+            // 
+            // lblShow
+            // 
+            resources.ApplyResources(this.lblShow, "lblShow");
+            this.lblShow.BackColor = System.Drawing.Color.IndianRed;
+            this.lblShow.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblShow.Name = "lblShow";
+            this.lblShow.UseWaitCursor = true;
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseWaitCursor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseWaitCursor = true;
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseWaitCursor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // mainForm
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblShow);
+            this.Controls.Add(this.btnresult);
+            this.Controls.Add(this.btndot);
+            this.Controls.Add(this.btnchu);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btrncheng);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnpluss);
+            this.Controls.Add(this.btnjian);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Name = "mainForm";
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btnjian;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btrncheng;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btnchu;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btndot;
+        private System.Windows.Forms.Button btnresult;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button btnpluss;
+        private System.Windows.Forms.Label lblShow;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jisuanqi
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        //字符串声明
+        //string tempStrNum1 = "";
+        string tempStrOperator = "";
+
+        //数字声明
+        double num1 = 0;
+        double num2 = 0;
+        double result = 0;
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void ButtonNum_Click(object sender, EventArgs e)
+        {
+            Button buttonNumber = (Button)sender;
+
+            if (lblShow.Text == "0")
+            {
+                //判断输入的是否为小数点。
+                if (buttonNumber.Text != ".")
+                    lblShow.Text = buttonNumber.Text;
+                else
+                    lblShow.Text += buttonNumber.Text;
+                //tempStrNum1 = str1;
+            }
+            else
+            {
+                lblShow.Text += buttonNumber.Text;
+                //tempStrNum1 += str1;
+                //lblShow.Text = "9";
+            }//if (lblShow.Text == "0")
+        }
+
+        //
+        //操作符按钮方法
+        //
+        private void ButtonOperator_Click(object sender, EventArgs e)
+        {
+            Button buttonOperator = (Button)sender;
+            //tempStrOperator = buttonOperator.Text;
+            if (buttonOperator.Text != "=")
+            {
+                //
+                //查找运算符
+                //
+                switch (buttonOperator.Text)
+                {
+                    case "+":
+                        {
+                            tempStrOperator = buttonOperator.Text;
+                            lblShow.Text += tempStrOperator;
+                        } break;
+                    case "-":
+                        {
+                            tempStrOperator = buttonOperator.Text;
+                            lblShow.Text += tempStrOperator;
+                        }
+                        break;
+                    case "×":
+                        {
+                            tempStrOperator = buttonOperator.Text;
+                            lblShow.Text += tempStrOperator;
+                        }
+                        break;
+                    case "÷":
+                        {
+                            tempStrOperator = buttonOperator.Text;
+                            lblShow.Text += tempStrOperator;
+                        }
+                        break;
+                } 
+            }
+            else if (buttonOperator.Text == "=")
+            {
+
+                //string tempStrNum = lblShow.Text;
+                if (lblShow.Text != "")
+                {
+                    int j = 0;
+                    for (int i = 0; i < lblShow.Text.Length; i++)
+                    {
+                        //if(tempStrOperator!="")
+                        if (lblShow.Text[i] == '+' || lblShow.Text[i] == '-' || lblShow.Text[i] == '×' || lblShow.Text[i] == '÷')
+                        {
+                            j = i;
+                            break;
+                        }
+
+                    }//for(int i = 0; i < lblShow.Text.Length; i++)
+
+                    if (j == 0)
+                        num1 = Convert.ToDouble(lblShow.Text.Substring(0));
+                    else
+                    {
+                        num1 = Convert.ToDouble(lblShow.Text.Substring(0, j));
+                        if (lblShow.Text.Length - 1 >= j + 1)
+                            num2 = Convert.ToDouble(lblShow.Text.Substring(j + 1));
+                    }
+                    //lblShow.Text = lblShow.Text.Substring(j+1);
+                    result = num1 + num2;
+                    string str1 = result.ToString();
+                    lblShow.Text += buttonOperator.Text;//附上等于号
+                                                        //lblShow.Text = result.ToString();//出现了小数点不能正确运算的问题
+                                                        //lblShow.Text += num2.ToString();
+                                                        //lblShow.Text = lblShow.Text+'\n'+ num1.ToString()+'\n'+num2.ToString()+'\n'+result.ToString();
+
+
+
+                    switch (tempStrOperator)
+                    {
+                        case "+": 
+                            { 
+                                result = num1 + num2;
+                                lblShow.Text += Convert.ToString(result);
+                            } break;
+                        case "-": 
+                            {
+                                result = num1 - num2;
+                                lblShow.Text += Convert.ToString(result);
+                            } break;
+                        case "×":
+                            {
+                                result = num1 * num2;
+                                lblShow.Text += Convert.ToString(result);
+                            } break;
+                        case "÷":
+                            {
+                                //判断除数是否为零
+                                if (num2 != 0)
+                                {
+                                    result = num1 / num2;
+                                    lblShow.Text += Convert.ToString(result);
+                                }
+                                else
+                                    lblShow.Text = "不能除以零";
+                            }
+                            break;
+                    }//switch (tempStrOperator)
+
+                    //
+                    //结果现显示
+                    //
+
+
+                    //lblShow.Text += Convert.ToString(result);
+                    //lblShow.Text += Convert.ToString(result);
+                    //lblShow.Text += str1;
+
+                    //witch()
+
+                }
+
+                //运算符标记清零
+                tempStrOperator = "";
+            }
+        }
+        //
+        //运算函数
+        //
+        
+        //
+        //退格按钮
+        //
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string tempStr = lblShow.Text;
+            //如果显示器内字符串长度大于0，则执行退格操作
+            if (tempStr.Length > 0)
+            {                
+                lblShow.Text = tempStr.Substring(0, tempStr.Length - 1);
+                //如果数字暂存区内字符串长度大于0，则执行退格操作
+                //if (tempStrNum1.Length > 0)
+                  //  tempStrNum1 = tempStrNum1.Substring(0, tempStrNum1.Length - 1);
+            }
+        }
+        //
+        //清屏按钮
+        //
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lblShow.Text = null;
+            tempStrOperator = null;
+            result = 0;
+            num1 = 0;
+            num2 = 0;
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
